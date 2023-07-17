547984a1a5c0bc929c9b739a2094fdf50352d112
Zackary Santana <zackzackyack@gmail.com>
2023-07-16 23:42:24 -0400

    Colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,15 @@
 module.exports = {
     content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                html: "#f16524",
+                css: "#2965f1",
+                js: "#f0db4f",
+                ts: "#007acc",
+                jsdoc: "#006cb7",
+            },
+        },
     },
     plugins: [],
 };
