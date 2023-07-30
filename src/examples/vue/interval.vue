bcd9404487031370ef7cc4b92d10a934f5a1a931
ZackarySantana <zackzackyack@gmail.com>
2023-07-30 10:51:43 -0400

    Cleanup on vue component

--- src/examples/vue/interval.vue
+++ src/examples/vue/interval.vue
@@ -7,12 +7,16 @@ export default {
     data() {
         return {
             counter: 0,
+            intervalId: 0,
         };
     },
     mounted() {
-        setInterval(() => {
+        this.intervalId = setInterval(() => {
             this.counter++;
-        }, 1000);
+        }, 1000) as any as number;
+    },
+    unmounted() {
+        clearInterval(this.intervalId);
     },
 };
 </script>
