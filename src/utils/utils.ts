15c906469ece2f07d97c0ab833996db1d6a1d1fa
Zackary Santana <zackzackyack@gmail.com>
2023-08-03 21:19:01 -0400

    Starting fetch

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,26 +2,48 @@ import fs from "fs";
 
 export const examples: {
     [key: string]: {
+        name?: string;
         desc: string;
+        shortDesc: string;
         tags: string[];
     };
     descs;
 } = {
     styling: {
         desc: "How custom styling styling can be done in every framework.",
+        shortDesc:
+            "How to style different components native to each framework.",
         tags: ["Frontend"],
     },
     counter: {
-        desc: "An interactive counter with a decrement and increment button. Note that for SSR, the counter will not be interactive.",
-        tags: ["Frontend"],
+        desc: "Interactive counter with two buttons to increment and decrement the counter. Since this is interactive, the buttons will not work when SSR.",
+        shortDesc:
+            "Interactive counter with two buttons to increment and decrement the counter.",
+        tags: ["Frontend", "Interactive"],
     },
     interval: {
-        desc: "A 1 second interval (timer) that updates a number by 1. Note that for SSR, the number will not update.",
-        tags: ["Frontend"],
+        desc: "Async 1 second interval/timer that increments a number by 1. Since this async, the interval will not start when SSR.",
+        shortDesc:
+            "Async 1 second interval/timer that increments a number by 1.",
+        tags: ["Frontend", "Async"],
     },
     todo: {
-        desc: "A todo list that allows you to add and remove items. Note that for SSR, the list will not be interactive.",
-        tags: ["Frontend"],
+        desc: "Interactive todo list that starts with some initial items when created/mounted. Since the initial items are added when created/mounted, they will not display when SSR. As well, the buttons will not work when SSR.",
+        shortDesc:
+            "Interactive todo list that starts with some initial items when created/mounted.",
+        tags: ["Frontend", "Interactive"],
+    },
+    fetch: {
+        desc: "Async call to fetch some data from an API. Since this is async, the data will not be fetched when SSR.",
+        shortDesc: "Async call to fetch some data from an API.",
+        tags: ["Frontend", "Async"],
+    },
+    crud: {
+        name: "CRUD",
+        desc: "Simple CRUD (Create, Read, Update, Delete) API that reads from a JSON file.",
+        shortDesc:
+            "Simple CRUD (Create, Read, Update, Delete) API that reads from a JSON file.",
+        tags: ["Backend", "API"],
     },
 };
 
