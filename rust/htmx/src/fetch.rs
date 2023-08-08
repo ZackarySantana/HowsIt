adb1a054d1a2f03456b1304cf7b8e8db9f6d0e9b
ZackarySantana <zackzackyack@gmail.com>
2023-08-08 05:35:47 +0100

    Switch to dealing with json rather than raw strings

--- rust/htmx/src/fetch.rs
+++ rust/htmx/src/fetch.rs
@@ -2,27 +2,30 @@ use actix_web::{get, HttpRequest, HttpResponse};
 use rand::seq::SliceRandom;
 use std::env;
 
+fn get_api() -> String {
+    return env::var("API_URL")
+        .unwrap()
+        .trim_end_matches("/")
+        .to_string();
+}
+
 fn element(item: String) -> String {
     item
 }
 
 #[get("/htmx/rust/fetch")]
 pub async fn get(_: HttpRequest) -> HttpResponse {
-    let api = env::var("API_URL").unwrap();
-    let api = api.trim_end_matches("/");
+    let api = get_api();
     let resp = reqwest::get(&format!("{}/api/fetch", api))
         .await
         .unwrap()
         .text()
         .await
         .unwrap();
-    let items = resp
-        .trim_start_matches('[')
-        .trim_end_matches(']')
-        .split("\",\"")
-        .map(|s| s.to_string())
-        .collect::<Vec<String>>();
+
+    let items: Vec<String> = serde_json::from_str(&resp).unwrap();
     let item = items.choose(&mut rand::thread_rng()).unwrap().to_string();
+
     return HttpResponse::Ok()
         .content_type("text/html; charset=utf-8")
         .body(element(item));
