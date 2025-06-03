bee92483adfff17330fb05d0d658c67997726c8d
ZackarySantana <64446617+ZackarySantana@users.noreply.github.com>
2025-06-02 22:17:20 -0400

    feat: add health for rubber

--- rubber/src/main.rs
+++ rubber/src/main.rs
@@ -1,23 +1,35 @@
-use actix_web::{middleware, App, HttpServer};
+use actix_web::{middleware, web, App, HttpResponse, HttpServer, Result};
 use dotenv::dotenv;
 use log::info;
+use std::env;
 mod counter;
 mod fetch;
 mod interval;
 mod todo;
+
+async fn health() -> Result<HttpResponse> {
+    Ok(HttpResponse::Ok().json(serde_json::json!({
+        "status": "ok",
+        "service": "rubber"
+    })))
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     env_logger::init_from_env(env_logger::Env::default().default_filter_or("info"));
 
-    info!("starting http server at http://[::]:3001");
+    let port = env::var("PORT").unwrap_or_else(|_| "3001".to_string());
+    let addr = format!("[::]:{}", port);
+    
+    info!("starting http server at http://{}", addr);
 
-    let addr = "[::]:3001".parse::<std::net::SocketAddr>().unwrap();
+    let socket_addr = addr.parse::<std::net::SocketAddr>().unwrap();
 
     HttpServer::new(|| {
         App::new()
             .wrap(middleware::Logger::default())
+            .route("/", web::get().to(health))
             .service(counter::post)
             .service(counter::get)
             .service(interval::post)
@@ -27,7 +39,7 @@ async fn main() -> std::io::Result<()> {
             .service(todo::delete)
             .service(fetch::get)
     })
-    .bind(addr)?
+    .bind(socket_addr)?
     .run()
     .await
 }
